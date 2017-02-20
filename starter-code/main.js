0e40bb4ed387c70aeb7b99f01a462a70223296c8
Rachel Linthicum <rachellinthicum@Rachels-Air.lan>
2017-02-20 15:18:44 -0500

    created if else

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,3 +3,18 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+if (cardTwo === cardFour) {
+	alert('You found a match!');
+} else {
+	alert('Sorry, try again.');
+}
+if (cardOne === cardTwo) {
+	alert('You found a match!');
+} else {
+	alert('Sorry, try again.');
+}
+if (cardThree === cardFour) {
+	alert('You found a match!');
+} else {
+	alert('Sorry, try again.');
+}
