16724e81447e747b4851ab571db6c12328cd8e17
Rachel Linthicum <rachellinthicum@Rachels-Air.lan>
2017-02-21 17:36:57 -0500

    created cards js

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,7 +3,7 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
-if (cardTwo === cardFour) {
+/* if (cardTwo === cardFour) {
 	alert('You found a match!');
 } else {
 	alert('Sorry, try again.');
@@ -17,4 +17,21 @@ if (cardThree === cardFour) {
 	alert('You found a match!');
 } else {
 	alert('Sorry, try again.');
+} */
+var createCards = function() {
+var board = document.getElementById('game-board');
+board.className = "board";
+var cardOne = document.createElement('div');
+cardOne.className = "card";
+var cardTwo = document.createElement('div');
+cardTwo.className = "card";
+var cardThree = document.createElement('div');
+cardThree.className = "card";
+var cardFour = document.createElement('div');
+cardFour.className = "card";
+board.appendChild(cardOne);
+board.appendChild(cardTwo);
+board.appendChild(cardThree);
+board.appendChild(cardFour);
 }
+createCards();
