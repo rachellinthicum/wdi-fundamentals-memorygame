877513487d0db510f4776e707282d915f498fcb0
Rachel Linthicum <rachellinthicum@Rachels-Air.lan>
2017-02-22 15:04:22 -0500

    completed final project

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,9 +1,9 @@
 // console.log("JS file is connected to HTML! Woo!")
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+/*var cardOne = 'queen';
+var cardTwo = 'queen';
+var cardThree = 'king';
+var cardFour = 'king';*/
 
 /*if (cardTwo === cardFour) {
 	alert('You found a match!');
@@ -41,12 +41,42 @@ board.appendChild(cardFour);
 }
 createCards();*/
 
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [];
+
 var board = document.getElementById('game-board');
 function createCards() {
 	for (var i=0; i<4; i++) {
 		var cardElement = document.createElement('div');
 		cardElement.className = 'card';
+		cardElement.setAttribute('data-card', cards[i]);
+		cardElement.addEventListener('click', isTwoCards);
 		board.appendChild(cardElement);
 	}
 }
 createCards();
+
+function isMatch(cards) {
+	if (cards[0] === cards[1]) {
+	alert('You found a match!');
+    } else {
+	alert('Sorry, try again.');
+    
+    }
+}
+
+function isTwoCards() {
+	cardsInPlay.push(this.getAttribute('data-card'));
+	console.log(this.getAttribute('data-card'));
+	if (this.getAttribute('data-card') === 'king') {
+		this.innerHTML = '<img src="King.png">';
+	} else {
+		this.innerHTML = '<img src="Queen.png">';
+	}
+	if (cardsInPlay.length === 2) {
+		isMatch(cardsInPlay);
+		cardsInPlay = [];
+	}
+}
+/*I needed a lot of help from the Assignment Solutions to get through the last
+part of this project.*/
